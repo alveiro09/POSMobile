65603e45bdf8b9a5f89db4f0a34efe3938d5d83c
Cristian Mejia <alveiro09@hotmail.com>
2017-11-13 20:55:37 -0500

    Pantall de creaion de usuario

--- app/src/main/java/com/posmobile/CrearUsuarios.java
+++ app/src/main/java/com/posmobile/CrearUsuarios.java
@@ -6,6 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.posmobile.modelo.Usuario;
 
 public class CrearUsuarios extends AppCompatActivity {
 
@@ -13,10 +18,55 @@ public class CrearUsuarios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_usuarios);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar;
+        final EditText edtNombreUsuario, edtNumeroIdentificacion, edtPrimerNombre, edtSegundoNombre;
+        final EditText edtPrimerApellido, edtSegundoApellido, edtContrasena;
+        Button btnCancelar, btnGuardar;
+        Usuario usuario;
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        edtNombreUsuario = (EditText) findViewById(R.id.edtNombreUsuario);
+        edtNumeroIdentificacion = (EditText) findViewById(R.id.edtNumeroIdentificacion);
+        edtPrimerNombre = (EditText) findViewById(R.id.edtPrimerNombre);
+        edtSegundoNombre = (EditText) findViewById(R.id.edtSegundoNombre);
+        edtPrimerApellido = (EditText) findViewById(R.id.edtPrimerApellido);
+        edtSegundoApellido = (EditText) findViewById(R.id.edtSegundoApellido);
+        edtContrasena = (EditText) findViewById(R.id.edtContrasena);
+
+        btnGuardar = (Button) findViewById(R.id.bntGuardar);
+        btnCancelar = (Button) findViewById(R.id.bntCancelar);
+
         setSupportActionBar(toolbar);
 
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                edtNumeroIdentificacion.setText("");
+                edtNombreUsuario.setText("");
+                edtPrimerNombre.setText("");
+                edtSegundoNombre.setText("");
+                edtPrimerApellido.setText("");
+                edtSegundoApellido.setText("");
+                edtContrasena.setText("");
+            }
+        });
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Usuario usuario = new Usuario();
+                usuario.setNombreUsuario(edtNombreUsuario.getText().toString());
+                usuario.setNumeroIdentificacion(edtNombreUsuario.getText().toString());
+                usuario.setPrimerApellido(edtNombreUsuario.getText().toString());
+                usuario.setSegundoNombre(edtNombreUsuario.getText().toString());
+                usuario.setPrimerApellido(edtNombreUsuario.getText().toString());
+                usuario.setSegundoApellido(edtNombreUsuario.getText().toString());
+                usuario.setContrasena(edtNombreUsuario.getText().toString());
+
+                // Toast.makeText(this, usuario.getPrimerNombre(),Toast.LENGTH_LONG);
+            }
+        });
     }
-
 }
+
---
+++ app/src/main/java/com/posmobile/modelo/Usuario.java
@@ -0,0 +1,75 @@
+package com.posmobile.modelo;
+
+/**
+ * Created by personal on 13/11/2017.
+ */
+
+public class Usuario {
+    private String numeroIdentificacion;
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String nombreUsuario;
+    private String contrasena;
+
+    public Usuario(){
+
+    }
+
+    public String getNumeroIdentificacion() {
+        return numeroIdentificacion;
+    }
+
+    public void setNumeroIdentificacion(String numeroIdentificacion) {
+        this.numeroIdentificacion = numeroIdentificacion;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
